05ca8453fcf8716199b400dcdb46061beaec0eac
Rinkit Adhana <therinkit@gmail.com>
2025-01-20 03:56:11 +0530

    Update MiddleHead component to include 'select-none' class for improved text selection behavior

--- src/Components/MiddleHead.tsx
+++ src/Components/MiddleHead.tsx
@@ -1,6 +1,6 @@
 const MiddleHead = () => {
   return (
-    <div className=" flex items-center">
+    <div className=" select-none flex items-center">
       <div className=" text-9xl  font-Canopee text-center bg-zinc-800 w-full my-10 text-[#E9E4DB] p-2">
         <p>Are We Truly Safe from Crime?</p>
       </div>
