ddf4717f8c2c2db6e880b24b80ead4d2244effe2
Rinkit Adhana <therinkit@gmail.com>
2025-01-20 03:22:07 +0530

    made new comp for the end

---
+++ src/Components/TheEnd.tsx
@@ -0,0 +1,18 @@
+const TheEnd = () => {
+  return (
+    <div className=" select-none flex items-center my-8">
+      <div className="flex-grow space-y-0.5">
+        <hr className="border-t-[5px] border-black/85" />
+        <hr className="border-t-[2px] border-black/90" />
+      </div>
+      <span className="mx-2 text-7xl font-OldLondon  ">The End</span>
+
+      <div className="flex-grow space-y-0.5">
+        <hr className="border-t-[5px] border-black/85" />
+        <hr className="border-t-[2px] border-black/90" />
+      </div>
+    </div>
+  )
+}
+
+export default TheEnd
