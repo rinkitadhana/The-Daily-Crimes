9cee81884cf03d4b0a566b731e371cd63804e251
Rinkit Adhana <therinkit@gmail.com>
2025-01-20 03:18:05 +0530

    footer enhanced

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,23 +2,39 @@ import Slider from "./Slider"
 
 const Footer = () => {
   return (
-    <div>
+    <div className="  select-none">
       <Slider />
       <div className="py-7 font-Canopee text-2xl  flex flex-row justify-between px-6 ">
-        <a target="_blank" href="https://therinkit.online">
-          therinkit ©
+        <a
+          className=" hover:opacity-75 transition-colors duration-300"
+          target="_blank"
+          href="https://therinkit.online"
+        >
+          The Daily Crimes ©
         </a>
 
         <div className=" flex gap-2 ">
-          <a target="_blank" href="https://x.com/rnkktt">
+          <a
+            className=" hover:opacity-75 transition-colors duration-300"
+            target="_blank"
+            href="https://x.com/damnGruz"
+          >
             Twitter
           </a>
           <span>•</span>
-          <a target="_blank" href="https://github.com/rinkitadhana">
+          <a
+            className=" hover:opacity-75 transition-colors duration-300"
+            target="_blank"
+            href="https://github.com/rinkitadhana"
+          >
             Github
           </a>
           <span>•</span>
-          <a target="_blank" href="https://www.linkedin.com/in/rinkitadhana/">
+          <a
+            className=" hover:opacity-75 transition-colors duration-300"
+            target="_blank"
+            href="https://www.linkedin.com/in/rinkitadhana"
+          >
             LinkedIn
           </a>
         </div>
--- src/Components/Slider.tsx
+++ src/Components/Slider.tsx
@@ -1,9 +1,12 @@
 const Slider = () => {
   return (
-    <div className=" marquee-container">
+    <div className=" marquee-container select-none">
       <div className="marquee">
-        Let's Create Something Together{" "}
-        <a href="mailto:therinkit@gmail.com">Email ME</a>.
+        Together, We Can Transform Our Nation,{" "}
+        <a target="_blank" href="https://x.com/damnGruz">
+          Follow Me
+        </a>
+        .
       </div>
     </div>
   )
