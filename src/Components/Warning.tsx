0806d5b8f31179b80e3a55842cfdfcec9377d362
Rinkit Adhana <therinkit@gmail.com>
2025-02-15 19:43:07 +0530

    message fixed

--- src/Components/Warning.tsx
+++ src/Components/Warning.tsx
@@ -8,9 +8,9 @@ const Warning = () => {
         <section className=" md:hidden fixed top-10 z-50 w-full m-2 ">
           <div className=" flex flex-col items-center gap-4 bg-zinc-800 text-white p-4 font-sans w-[50%] mx-auto border rounded-xl  border-zinc-800 shadow-2xl">
             <p className=" text-xl text-center ">
-              For the best UPI experience, please open this website on a
-              desktop. The site contains numerous elements that may lose their
-              visual appeal if fully optimized for responsiveness.
+              For the best UI experience, please open this website on a desktop.
+              The site contains numerous elements that may lose their visual
+              appeal if fully optimized for responsiveness.
             </p>
             <button
               onClick={() => setVis((prev) => !prev)}
